7b1002a0490fb984b515c6bec3ab1b7178d2940f
Ben Banting <bbanting@tuta.io>
2023-04-26 21:46:35 -0600

    Put actual API endpoint in and added authorization header

--- typebetter.c
+++ typebetter.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <curl/curl.h>
 
-#define API_URL "https://www.google.ca"
+#define API_URL "https://api.esv.org/v3/passage/text/?q=John+3%3A16&include-headings=False&include-footnotes=False&include-verse-numbers=False&include-short-copyright=False&include-passsage-references=False&include-first-verse-numbers=False"
 
 
 struct response_data {
@@ -33,10 +33,16 @@ int main() {
 	CURLcode res_code;
 
 	struct response_data res_data = {0};
+	struct curl_slist *headers = NULL;
 	curl = curl_easy_init();
 
 	if (curl) {
+		headers = curl_slist_append(headers, "Authorization: <secret>");
+		if (headers == NULL) {
+			return EXIT_FAILURE;
+		}
 		curl_easy_setopt(curl, CURLOPT_URL, API_URL);
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&res_data);
 
@@ -49,7 +55,8 @@ int main() {
 
 		printf("Here's the response data: %s\n", res_data.contents);
 			
-
+		
+		curl_slist_free_all(headers);
 		curl_easy_cleanup(curl);
 		free(res_data.contents);
 	}
