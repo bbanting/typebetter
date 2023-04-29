df00247e0b7ba1bbdb53f5aef493b4b541948df2
Ben Banting <bbanting@tuta.io>
2023-04-28 20:52:37 -0600

    Put auth token in text file, made get_auth_header()

--- typebetter.c
+++ typebetter.c
@@ -28,38 +28,61 @@ size_t write_callback(char *data, size_t size, size_t nmemb, void *data_memory) 
 }
 
 
+void get_auth_header(char *str) {
+	FILE *fp = fopen("token.txt", "r");
+	if (fp == NULL) {
+		fprintf(stderr, "Error opening token file.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	char token[100];
+	fgets(token, 100, fp);
+	token[strlen(token) - 1] = 0;
+
+	strcat(str, "Authorization: ");
+	strcat(str, token);
+} 
+
+
 int main() {
 	CURL *curl;
+	curl = curl_easy_init();
+	if (curl == NULL) {
+		fprintf(stderr, "Curl init failure.\n");
+		return EXIT_FAILURE;
+	}
+
 	CURLcode res_code;
 
 	struct response_data res_data = {0};
 	struct curl_slist *headers = NULL;
-	curl = curl_easy_init();
 
-	if (curl) {
-		headers = curl_slist_append(headers, "Authorization: <secret>");
-		if (headers == NULL) {
-			return EXIT_FAILURE;
-		}
-		curl_easy_setopt(curl, CURLOPT_URL, API_URL);
-		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&res_data);
+	char auth_header[100];
+	get_auth_header(auth_header);
+	headers = curl_slist_append(headers, auth_header);
 
-		res_code = curl_easy_perform(curl);
-
-		if (res_code != CURLE_OK) {
-			fprintf(stderr, "Curl failure: %s\n", curl_easy_strerror(res_code));
-			return EXIT_FAILURE;
-		}
-
-		printf("Here's the response data: %s\n", res_data.contents);
-			
-		
-		curl_slist_free_all(headers);
-		curl_easy_cleanup(curl);
-		free(res_data.contents);
+	if (headers == NULL) {
+		return EXIT_FAILURE;
 	}
+	
+	curl_easy_setopt(curl, CURLOPT_URL, API_URL);
+	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&res_data);
+
+	res_code = curl_easy_perform(curl);
+
+	if (res_code != CURLE_OK) {
+		fprintf(stderr, "Curl failure: %s\n", curl_easy_strerror(res_code));
+		return EXIT_FAILURE;
+	}
+
+	printf("Here's the response data: %s\n", res_data.contents);
+		
+	
+	curl_slist_free_all(headers);
+	curl_easy_cleanup(curl);
+	free(res_data.contents);
 
 	return EXIT_SUCCESS;
 }
